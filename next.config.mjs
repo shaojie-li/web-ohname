1edce31b0d9d8220cb8f38552410884aba0823ec
leeshaojie <shaojie.li@imaginelabs.space>
2024-05-22 13:39:09 +0800

    feat: telegram login

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    // output: "export", // 启用静态导出
+
     images: {
         remotePatterns: [
             {
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import Header from "@/components/header";
 import "./globals.css";
 import { openGraphImage } from "./shared-metadata";
 import SideBar from "@/components/sideBar";
+import Script from "next/script";
 
 export const metadata: Metadata = {
     title: "oh-name",
@@ -27,12 +28,26 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
+            <Script
+                async
+                src="https://telegram.org/js/telegram-widget.js?22"
+                data-telegram-login="testsosovalueBot"
+                data-size="large"
+                data-onauth="onTelegramAuth(user)"
+                data-request-access="write"
+            />
+            <Script
+                id="c"
+                dangerouslySetInnerHTML={{
+                    __html: 'function onTelegramAuth(user) {console.log("Logged in as ", user)',
+                }}
+            />
             <body>
                 <div className="flex flex-col">
                     <div className="flex min-h-screen">
                         <SideBar header={<Header />} />
                         <div className="flex-1 p-6">
-                            <div className="bg-[#232627] rounded-3xl h-full">{children}</div>
+                            <div className="p-6 bg-[#232627] rounded-3xl h-full">{children}</div>
                         </div>
                     </div>
                 </div>
